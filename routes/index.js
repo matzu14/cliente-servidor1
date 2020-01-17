4e997c01e5c3c3aca71afb3e056be6c5df9d4ac7
Matzu <maximilianoga38@gmail.com>
2020-01-17 10:14:21 -0600

    17 enero

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mai paich' });
+  res.render('index', { title: 'C/S Course' });
+});
+
+router.get('/prueba', function(req, res){
+  res.send('<h1> Esto es una prueba <h1>');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/buena_onda', function(req, res, next) {
+  res.render('users/minipractica',{ numero: 'todos menos el oscar'});
+});
+
+// router.get('/buena_onda', function(req, res, next) {
+//   res.render('minipractica');
+// });
+
 module.exports = router;
